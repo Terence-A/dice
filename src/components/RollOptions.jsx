fca8ed5f5aa4c2b03eb1c690ebd5112e1c2cbe53
terenceDev <terence.arseneault@hotmail.com>
2024-02-15 21:09:32 -0400

    set the dice rolling button and the dice pieces to random dice between 1 and 6

--- src/components/RollOptions.jsx
+++ src/components/RollOptions.jsx
@@ -1,10 +1,45 @@
-const RollOptions = () => {
+const RollOptions = ({ setDiceRolled }) => {
+  const handleRollClick = () => {
+    setDiceRolled({
+      firstDie: Math.floor(Math.random() * 5),
+      secondDie: Math.floor(Math.random() * 5),
+      thirdDie: Math.floor(Math.random() * 5),
+      forthDie: Math.floor(Math.random() * 5),
+      fifthDie: Math.floor(Math.random() * 5),
+    });
+  };
+
   return (
     <section>
-      <div className="flex  w-[500px] mx-auto justify-between pt-11">
-        <button className="text-3xl text-stone-50 w-48 bg-green-900 hover:bg-green-700 font-bold px-8 py-4 border-4 border-stone-900 rounded-xl shadow-md hover:shadow-lg hover:shadow-stone-900 shadow-stone-900">
+      <div
+        className="flex
+        w-[500px] 
+        mx-auto 
+        justify-between 
+        pt-11"
+      >
+        <button
+          className="text-3xl
+         text-green-100 
+         w-48 
+         bg-green-900
+          hover:bg-green-700
+           font-bold 
+           px-8 
+           py-4 
+           border-4
+            border-stone-900
+             rounded-xl
+              shadow-md
+               hover:shadow-lg
+                hover:shadow-stone-900
+                 hover:text-green-100
+                  shadow-stone-900"
+          onClick={handleRollClick}
+        >
           Roll
         </button>
+
         <button className="text-3xl w-48  text-stone-50 bg-red-900 hover:bg-red-700 border-4 border-stone-900 font-bold px-8 py-4 rounded-xl shadow-md hover:shadow-lg hover:shadow-stone-900 shadow-stone-900">
           Take It
         </button>
--- src/diceData.jsx
+++ src/diceData.jsx
@@ -1,33 +1,40 @@
+import diceOne from "./assets/diceOne.png";
+import diceTwo from "./assets/diceTwo.png";
+import diceThree from "./assets/diceThree.png";
+import diceFour from "./assets/diceFour.png";
+import diceFive from "./assets/diceFive.png";
+import diceSix from "./assets/diceSix.png";
+
 const diceData = [
   {
     id: 1,
     num: 1,
-    image: "diceOne",
+    image: diceOne,
   },
   {
     id: 2,
     num: 2,
-    image: "diceTwo",
+    image: diceTwo,
   },
   {
     id: 3,
     num: 3,
-    image: "diceThree",
+    image: diceThree,
   },
   {
     id: 4,
     num: 4,
-    image: "diceFour",
+    image: diceFour,
   },
   {
     id: 5,
     num: 5,
-    image: "diceFive",
+    image: diceFive,
   },
   {
     id: 6,
     num: 6,
-    image: "diceSix",
+    image: diceSix,
   },
 ];
 
